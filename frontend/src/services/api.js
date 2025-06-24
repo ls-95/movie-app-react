4de1dd7947f08e97e07f3f70714d8a4fae44d9ed
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-06-24 14:30:45 +0200

    fix spelling mistakes and add error and loading messages

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,6 @@
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
-console.log("env", import.meta.env);
-console.log("API_KEY", API_KEY);
-
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
