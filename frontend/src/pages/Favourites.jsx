140bbedcb1f08a1bccedf27700bb20b89f73a555
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-06-25 14:55:33 +0200

    Add movie context and favourites functionality

--- frontend/src/pages/Favourites.jsx
+++ frontend/src/pages/Favourites.jsx
@@ -1,6 +1,22 @@
 import "../css/Favourites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favourites() {
+  const { favourites } = useMovieContext();
+
+  if (favourites.length > 0) {
+    return (
+      <div>
+        <h2 className="favourites">Your Favourites</h2>
+        <div className="movies-grid">
+          {favourites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="favourites-empty">
       <h2>No Favourite Movies Yet</h2>
